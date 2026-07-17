91648b38f3b6da45971c943f96da821fde1a73c6
vobaolan <2500116096@vanlangsaigon.edu.vn>
2026-07-17 16:05:30 +0700

    Optimize production build configuration in vite.config.js for security

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,21 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    sourcemap: false, // Tắt hoàn toàn sourcemap để F12 không xem được code gốc
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  esbuild: {
+    drop: ['console', 'debugger'] // Xóa bỏ toàn bộ console.log và debugger khi build bản thật
   }
 })
